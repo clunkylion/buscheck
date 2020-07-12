a980929b6c78031af5739f356326d47061f8aeda
Jose <cote99salamanca@gmail.com>
2020-07-12 13:41:29 -0400

    se controlaron todas las excepciones para consumirlas del front

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,9 +2,14 @@
 
 namespace App\Exceptions;
 
+use ErrorException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\UnauthorizedException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -53,7 +58,26 @@ class Handler extends ExceptionHandler
     public function render($request, Throwable $exception)
     {   
         //in this place work with Model not found exception .....
-        
+        //dd($exception);
+        //die();
+        if ($exception instanceof ErrorException) {
+            return response()->json([
+                "message" => "Error en el servidor",
+                "status" => Response::HTTP_INTERNAL_SERVER_ERROR
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return response()->json([
+                "message" => "El método no es soportado en está ruta, puede ser POST, GET, PUT, PATCH, DELETE",
+                "status" => Response::HTTP_METHOD_NOT_ALLOWED
+            ], Response::HTTP_METHOD_NOT_ALLOWED);
+        }
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json([
+                "message" => "No encontrado",
+                "status" => Response::HTTP_NOT_FOUND
+            ], Response::HTTP_NOT_FOUND);
+        }
         return parent::render($request, $exception);
     }
 }
