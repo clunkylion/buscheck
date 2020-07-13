1960e0bdc83b0a240dd7eba68a69dd691dea17a8
jose-server-buscheck <cote99salamanca@gmail.com>
2020-07-13 01:55:12 +0000

    se agregaron nuevos elementos al metodo boot

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Passport\Passport;
+use Carbon\Carbon;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         Passport::routes();
+        Passport::$ignoreCsrfToken = true;
+        /*Passport::tokensExpireIn(Carbon::now()->addYears(20));
+        Passport::refreshTokensExpireIn(Carbon::now()->addYears(20));*/
+        Passport::cookie('user_cookie');
         //
     }
 }
