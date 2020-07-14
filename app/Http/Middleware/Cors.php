c1a670ee3e03639d5c2f0b4a12cac1291de9523e
Jose <cote99salamanca@gmail.com>
2020-07-14 17:21:16 -0400

    agregue el comentario en rama dev

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,6 +16,7 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
+            //probando la rama dev
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type', 'Authorization', 'X-Auth-Token', 'Accept', 'X-Requested-With', 'application/json')
